2996174739933a2824268a976663b06e3d403067
lmq4088 <352489099@qq.com>
2019-07-29 18:26:41 +0800

    121321

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import url("/common/uni.css");
+	@import url("/common/animate.css");
+	@import url("/common/icon.css");
+	@import url("/common/common.css");
+</style>
